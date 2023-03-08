7ba7a28114dd5c984314d52916e9fbd8b7fbb6ba
Tűri Erik <54734700+turierik@users.noreply.github.com>
2023-03-08 21:41:41 +0100

    2. gyakorlat

---
+++ Turi_Erik/N5/laravel_blog/database/seeders/DatabaseSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Seeders;
+
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\User;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     */
+    public function run(): void
+    {
+        for ($i = 1; $i <= 20; $i++){
+            User::factory()->create([
+                'email' => 'user' . $i .  '@szerveroldali.hu'
+            ]);
+        }
+    }
+}
